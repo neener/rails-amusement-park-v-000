5de06a9e4041699254201d60a8fc15ea5692af37
neener <janinemrosen@gmail.com>
2017-01-29 04:43:39 -0500

    done

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,15 +4,27 @@ class RidesController < ApplicationController
 		@ride = Ride.new
 	end
 
-	def create
-		@ride = Ride.new(ride_params)
-		if @ride.save
-			current_user.update(tickets: current_user.tickets - @ride.attraction.tickets, nausea: @ride.attraction.nausea_rating, happiness: @ride.attraction.happiness_rating)
-			redirect_to user_path(current_user)		
-		else
-			render 'new'
-		end
-	end
+	  def create
+	    @ride = Ride.new(ride_params)
+	    @ride.save
+	    if @ride.take_ride.class == String
+	      flash[:alert] = @ride.take_ride
+	    else
+	      flash[:alert] = "Thanks for riding the #{@ride.attraction.name}!"
+	    end
+	    redirect_to user_path(current_user)
+	  end
+
+	# def create
+	# 	# raise params.inspect
+	# 	@ride = Ride.new(ride_params)
+	# 	if @ride.save
+	# 		current_user.update(tickets: current_user.tickets - @ride.attraction.tickets, nausea: @ride.attraction.nausea_rating, happiness: @ride.attraction.happiness_rating)
+	# 		redirect_to user_path(current_user)		
+	# 	else
+	# 		render 'new'
+	# 	end
+	# end
 
 	private
 
